51b75ad5e08a3d6fd804f3a532999df30eaf56e5
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-02 18:29:46 +0200

    [refactor] cleaner secrets imports in import.sh

--- infra/scripts/import.sh
+++ infra/scripts/import.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 set -e
 
+# ---------------------------------------------------------
+# Script to import existing Azure resources into Terraform state
+# ----------------------------------------------------------
+
 SUB_ID="$TF_VAR_subscription_id"
 RG_N="$TF_VAR_resource_group_name"
 RG_ID="/subscriptions/$SUB_ID/resourceGroups/$RG_N"
 
 SA_N="$TF_VAR_storage_account_name"
-SA_ID="/subscriptions/$SUB_ID/resourceGroups/$RG_N/providers/Microsoft.Storage/storageAccounts/$SA_N"
+SA_ID="$RG_ID/providers/Microsoft.Storage/storageAccounts/$SA_N"
 
 CR_N="$TF_VAR_container_registry_name"
-CR_ID="/subscriptions/$SUB_ID/resourceGroups/$RG_N/providers/Microsoft.ContainerRegistry/registries/$CR_N"
+CR_ID="$RG_ID/providers/Microsoft.ContainerRegistry/registries/$CR_N"
 
 KV_N="${RG_N}-kv"
-KV_ID="/subscriptions/$SUB_ID/resourceGroups/$RG_N/providers/Microsoft.KeyVault/vaults/$KV_N"
+KV_ID="$RG_ID/providers/Microsoft.KeyVault/vaults/$KV_N"
 
 declare -a RESOURCES=(
-  "azurerm_resource_group.rg|az group show \
-    --name \"$RG_N\"|\"$RG_ID\""
-  "azurerm_storage_account.storage|az storage account show \
-    --name \"$SA_N\" \
-    --resource-group \"$RG_N\"|\"$SA_ID\""
-  "azurerm_container_registry.acr|az acr show \
-    --name \"$CR_N\" \
-    --resource-group \"$RG_N\"|\"$CR_ID\""
-  "azurerm_key_vault.kv|az keyvault show \
-    --name \"$KV_N\" \
-    --resource-group \"$RG_N\"|\"$KV_ID\""
+  "azurerm_resource_group.rg|az group show --name \"$RG_N\"|\"$RG_ID\""
+  "azurerm_storage_account.storage|az storage account show --name \"$SA_N\" --resource-group \"$RG_N\"|\"$SA_ID\""
+  "azurerm_container_registry.acr|az acr show --name \"$CR_N\" --resource-group \"$RG_N\"|\"$CR_ID\""
+  "azurerm_key_vault.kv|az keyvault show --name \"$KV_N\" --resource-group \"$RG_N\"|\"$KV_ID\""
 )
 
 for entry in "${RESOURCES[@]}"; do
@@ -44,60 +41,10 @@ for entry in "${RESOURCES[@]}"; do
 done
 
 
-# RESOURCE_GROUP_NAME="$TF_VAR_resource_group_name"
-# SUBSCRIPTION_ID="$TF_VAR_subscription_id"
-# RG_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME"
-
-# # Import Resource Group
-# if ! terraform state show azurerm_resource_group.rg &>/dev/null; then
-#   terraform import azurerm_resource_group.rg "$RG_ID"
-# else
-#   echo "Resource Group already managed by Terraform. Skipping import."
-# fi
-
-# # Import Storage Account
-# STORAGE_ACCOUNT_NAME="$TF_VAR_storage_account_name"
-# STORAGE_ACCOUNT_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT_NAME"
-# if az storage account show --name "$STORAGE_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
-#   if ! terraform state show azurerm_storage_account.storage &>/dev/null; then
-#     echo "Importing existing storage account..."
-#     terraform import azurerm_storage_account.storage "$STORAGE_ACCOUNT_ID"
-#   else
-#     echo "Storage account already managed by Terraform. Skipping import."
-#   fi
-# else
-#   echo "Storage account does not exist. Skipping import."
-# fi
-
-# # Import Container Registry
-# CONTAINER_REGISTRY_NAME="$TF_VAR_container_registry_name"
-# CONTAINER_REGISTRY_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.ContainerRegistry/registries/$CONTAINER_REGISTRY_NAME"
-# if az acr show --name "$CONTAINER_REGISTRY_NAME" --resource-group "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
-#   if ! terraform state show azurerm_container_registry.acr &>/dev/null; then
-#     echo "Importing existing container registry..."
-#     terraform import azurerm_container_registry.acr "$CONTAINER_REGISTRY_ID"
-#   else
-#     echo "Container registry already managed by Terraform. Skipping import."
-#   fi
-# else
-#   echo "Container registry does not exist. Skipping import."
-# fi
-
-# # Import Key Vault
-# KEY_VAULT_NAME="${RESOURCE_GROUP_NAME}-kv"
-# KEY_VAULT_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.KeyVault/vaults/$KEY_VAULT_NAME"
-# if az keyvault show --name "$KEY_VAULT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
-#   if ! terraform state show azurerm_key_vault.kv &>/dev/null; then
-#     echo "Importing existing key vault..."
-#     terraform import azurerm_key_vault.kv "$KEY_VAULT_ID"
-#   else
-#     echo "Key vault already managed by Terraform. Skipping import."
-#   fi
-# else
-#   echo "Key vault does not exist. Skipping import."
-# fi
-
 # ------------------------------------------------
+# Function to import Key Vault secrets into Terraform state
+# ------------------------------------------------
+
 import_kv_secret() {
   local tf_name="$1"        # Terraform resource name (e.g. acr_username)
   local secret_name="$2"    # Azure secret name (e.g. acr-admin-username)
