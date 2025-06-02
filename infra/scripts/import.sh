3bbf871514bf2dc120d48a7aa616bbf38acabfb1
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-02 15:26:30 +0200

    imports secrets in import.sh

--- infra/scripts/import.sh
+++ infra/scripts/import.sh
@@ -15,7 +15,8 @@ fi
 
 STORAGE_ACCOUNT_NAME="$TF_VAR_storage_account_name"
 STORAGE_ACCOUNT_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT_NAME"
-# Check if the storage account exists
+# 
+Check if the storage account exists
 if az storage account show --name "$STORAGE_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
   echo "Importing existing storage account..."
   terraform import azurerm_storage_account.storage "$STORAGE_ACCOUNT_ID"
@@ -43,3 +44,55 @@ if az keyvault show --name "$KEY_VAULT_NAME" --resource-group "$RESOURCE_GROUP_N
 else
   echo "Key vault does not exist. Skipping import."
 fi
+
+
+# Import secrets
+# 
+# 5. Store secrets in Key Vault
+# resource "azurerm_key_vault_secret" "acr_username" {
+#   name         = "acr-admin-username"
+#   value        = azurerm_container_registry.acr.admin_username
+#   key_vault_id = azurerm_key_vault.kv.id
+# }
+
+# resource "azurerm_key_vault_secret" "acr_password" {
+#   name         = "acr-admin-password"
+#   value        = azurerm_container_registry.acr.admin_password
+#   key_vault_id = azurerm_key_vault.kv.id
+# }
+
+# resource "azurerm_key_vault_secret" "blob_connection_string" {
+#   name         = "blob-storage-connection-string"
+#   value        = azurerm_storage_account.storage.primary_connection_string
+#   key_vault_id = azurerm_key_vault.kv.id
+# }
+
+# Import secrets
+# Import ACR username secret
+ACR_USERNAME_SECRET_NAME="acr-admin-username"
+ACR_USERNAME_SECRET_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.KeyVault/vaults/$KEY_VAULT_NAME/secrets/$ACR_USERNAME_SECRET_NAME"
+if az keyvault secret show --name "$ACR_USERNAME_SECRET_NAME" --vault-name "$KEY_VAULT_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
+  echo "Importing existing ACR username secret..."
+  terraform import azurerm_key_vault_secret.acr_username "$ACR_USERNAME_SECRET_ID"
+else
+  echo "ACR username secret does not exist. Skipping import."
+fi
+# Import ACR password secret
+ACR_PASSWORD_SECRET_NAME="acr-admin-password"
+ACR_PASSWORD_SECRET_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.KeyVault/vaults/$KEY_VAULT_NAME/secrets/$ACR_PASSWORD_SECRET_NAME"
+if az keyvault secret show --name "$ACR_PASSWORD_SECRET_NAME" --vault-name "$KEY_VAULT_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
+  echo "Importing existing ACR password secret..."
+  terraform import azurerm_key_vault_secret.acr_password "$ACR_PASSWORD_SECRET_ID"
+else
+  echo "ACR password secret does not exist. Skipping import."
+fi
+# Import blob storage connection string secret
+BLOB_CONNECTION_STRING_SECRET_NAME="blob-storage-connection-string"
+BLOB_CONNECTION_STRING_SECRET_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.KeyVault/vaults/$KEY_VAULT_NAME/secrets/$BLOB_CONNECTION_STRING_SECRET_NAME"
+if az keyvault secret show --name "$BLOB_CONNECTION_STRING_SECRET_NAME" --vault-name "$KEY_VAULT_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
+  echo "Importing existing blob storage connection string secret..."
+  terraform import azurerm_key_vault_secret.blob_connection_string "$BLOB_CONNECTION_STRING_SECRET_ID"
+else
+  echo "Blob storage connection string secret does not exist. Skipping import."
+fi
+# Import other secrets
