be37b292cd281ffcc6ee8e3697bf63d77dbf4062
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-02 14:48:52 +0200

    fixes wrong reference in import.sh

--- infra/scripts/import.sh
+++ infra/scripts/import.sh
@@ -3,15 +3,6 @@ set -e
 
 RESOURCE_GROUP_NAME="$TF_VAR_resource_group_name"
 SUBSCRIPTION_ID="$TF_VAR_subscription_id"
-RG_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME"
-
-# Check if the resource group exists
-if az group show --name "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
-  echo "Importing existing resource group..."
-  terraform import azurerm_resource_group.rg "$RG_ID"
-else
-  echo "Resource group does not exist. Skipping import."
-fi
 
 STORAGE_ACCOUNT_NAME="$TF_VAR_storage_account_name"
 STORAGE_ACCOUNT_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT_NAME"
