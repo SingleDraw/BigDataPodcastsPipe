74624a015f623acf29dd45c77ed23e76f7916b39
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-02 14:56:22 +0200

    removes wrong imports in import.sh

--- infra/scripts/import.sh
+++ infra/scripts/import.sh
@@ -3,16 +3,25 @@ set -e
 
 RESOURCE_GROUP_NAME="$TF_VAR_resource_group_name"
 SUBSCRIPTION_ID="$TF_VAR_subscription_id"
+# RG_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME"
 
-STORAGE_ACCOUNT_NAME="$TF_VAR_storage_account_name"
-STORAGE_ACCOUNT_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT_NAME"
-# Check if the storage account exists
-if az storage account show --name "$STORAGE_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
-  echo "Importing existing storage account..."
-  terraform import azurerm_storage_account.storage "$STORAGE_ACCOUNT_ID"
-else
-  echo "Storage account does not exist. Skipping import."
-fi
+# # Check if the resource group exists
+# if az group show --name "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
+#   echo "Importing existing resource group..."
+#   terraform import azurerm_resource_group.rg "$RG_ID"
+# else
+#   echo "Resource group does not exist. Skipping import."
+# fi
+
+# STORAGE_ACCOUNT_NAME="$TF_VAR_storage_account_name"
+# STORAGE_ACCOUNT_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT_NAME"
+# # Check if the storage account exists
+# if az storage account show --name "$STORAGE_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --subscription "$SUBSCRIPTION_ID" &>/dev/null; then
+#   echo "Importing existing storage account..."
+#   terraform import azurerm_storage_account.storage "$STORAGE_ACCOUNT_ID"
+# else
+#   echo "Storage account does not exist. Skipping import."
+# fi
 
 CONTAINER_REGISTRY_NAME="$TF_VAR_container_registry_name"
 CONTAINER_REGISTRY_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.ContainerRegistry/registries/$CONTAINER_REGISTRY_NAME"
