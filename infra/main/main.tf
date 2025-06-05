d721cc8bbf14957a44fa87fe120844ab49069549
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-06 01:01:25 +0200

    [workflow] fixes auth-type

--- infra/main/main.tf
+++ infra/main/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_key_vault" "kv" {
     azurerm_resource_group.rg
   ]
 
-  Admin access policy for the current user
+  # Admin access policy for the current user
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
