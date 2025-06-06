fee733b9119aa75c5aead393a11e7b60a157656b
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-06 11:10:44 +0200

    [workflow] fixes auth-type

--- infra/main/variables.tf
+++ infra/main/variables.tf
@@ -95,8 +95,3 @@ variable "tenant_id" {
   description = "Azure Tenant ID"
   type        = string
 }
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
