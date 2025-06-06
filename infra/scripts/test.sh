943ea55f65eea7393cfc6c8236ea29edce204890
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-06 13:28:55 +0200

    testing paths for bash

--- infra/scripts/test.sh
+++ infra/scripts/test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -e
 
-# # paths is relative to gh workflow working directory
-# UTILS_DIR="../scripts/utils"
-
+# ---------------------------------------------------------
 # Get the absolute path of the directory containing this script
+# and set the path to the utils directory
+# ---------------------------------------------------------
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 UTILS_DIR="$SCRIPT_DIR/utils"
+# ---------------------------------------------------------
 
 PROMPT="This is a test prompt for the test utility script."
 
-source "../scripts/utils/test-util.sh" "$PROMPT" "$PROMPT"
+source "$UTILS_DIR/test-util.sh" "$PROMPT" "$PROMPT"
 
 test_util "$PROMPT" "$PROMPT"
