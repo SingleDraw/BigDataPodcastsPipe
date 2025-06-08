b41e49bdeb74044c609e1029e192b0990beeced3
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-08 22:57:18 +0200

    [debug] az function deployment

--- azure-functions/save-logs-app/save-logs/__init__.py
+++ azure-functions/save-logs-app/save-logs/__init__.py
@@ -5,16 +5,16 @@ from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
 from azure.keyvault.secrets import SecretClient
 from azure.core.exceptions import ResourceExistsError
-from typing import Optional
+# from typing import Optional
 
 
 
-# Read environment variables
-AZURE_STORAGE_ACCOUNT = os.getenv("AZURE_STORAGE_ACCOUNT", "")
-KEY_VAULT_URL = os.getenv("AZURE_KEY_VAULT_URL", "")
-KEY_VAULT_SECRET_NAME = os.getenv("AZURE_KEY_VAULT_SECRET_NAME")
-CONTAINER_NAME = os.getenv("BLOB_CONTAINER_NAME", "aci-logs")
-FUNCTION_KEY = os.getenv("FUNCTION_KEY")
+# # Read environment variables
+# AZURE_STORAGE_ACCOUNT = os.getenv("AZURE_STORAGE_ACCOUNT", "")
+# KEY_VAULT_URL = os.getenv("AZURE_KEY_VAULT_URL", "")
+# KEY_VAULT_SECRET_NAME = os.getenv("AZURE_KEY_VAULT_SECRET_NAME")
+# CONTAINER_NAME = os.getenv("BLOB_CONTAINER_NAME", "aci-logs")
+# FUNCTION_KEY = os.getenv("FUNCTION_KEY")
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
