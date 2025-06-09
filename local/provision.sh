390228bb1a2e895ec90fef2f9f807cc4789c4f97
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-09 14:06:51 +0200

    [scripts] fixes tag addition in local provision script

--- local/provision.sh
+++ local/provision.sh
@@ -10,16 +10,22 @@
 
 TAG_MODE="latest" # Default tag mode for build and push workflows
 
+# Map for GitHub Actions workflows
 declare -A workflow_map=(
     [test]="azurelogin.yml"                                   # -- Login to Azure - test workflow  
     [cleanacr]="utils.clean-acr-registry.yml"                 # -- Clean up Azure Container Registry
     [bootstrap]="bootstrap.yml"             # Bootstrap workflow - creates the initial setup    
     [infra]="provision.yml"                 # Provisioning resources - triggered by bootstrap workflow too
-    [images]="build-and-push-scraper.yml"   # Build and push scraper image
     [adf]="deploy-adf-pipeline.yml"         # Deploy Azure Data Factory pipeline    
-    [buildtest]="build-and-push-test.yml"       # -- Build and push test image
     [pipetest]="deploy-adf-pipetest.yml"        # -- Deploy ADF pipeline for test
     [azfnlogs]="deploy-az-fn-logs-uploader.yml" # Deploy Azure Function for logs uploader
+)
+
+# Map for build and push workflows - these use tagMode flag
+# to specify the tag mode for the images being built and pushed.
+declare -A builders_map=(
+    [images]="build-and-push-scraper.yml"   # Build and push scraper image  
+    [buildtest]="build-and-push-test.yml"       # -- Build and push test image
 )
 
 if [ "$1" != "--env" ] || [ -z "$2" ]; then
@@ -29,9 +35,10 @@ fi
 
 env="$2"
 workflow_file="${workflow_map[$env]}"
+builder_file="${builders_map[$env]}"
 
-if [ -z "$workflow_file" ]; then
-    echo "Error: Unknown environment '$env'. Valid options: ${!workflow_map[*]}"
+if [ -z "$workflow_file" ] && [ -z "$builder_file" ]; then
+    echo "Error: Unknown environment '$env'. Valid options: ${!workflow_map[*]} or ${!builders_map[*]}"
     exit 1
 fi
 
@@ -41,10 +48,20 @@ source .env
 set +o allexport
 
 # Run the GitHub Actions workflow
-echo "Triggering GitHub Actions workflow '$workflow_file' for environment '$env'..."
-gh workflow run "$workflow_file" \
-    --repo "$GITHUB_REPOSITORY" # \
-    # -f tagMode="$TAG_MODE"
+running_workflow="${workflow_file:-$builder_file}"
+echo "Triggering GitHub Actions workflow '$running_workflow' for environment '$env'..."
+
+if [ -n "$workflow_file" ]; then
+    gh workflow run "$workflow_file" \
+        --repo "$GITHUB_REPOSITORY"
+elif [ -n "$builder_file" ]; then
+    gh workflow run "$builder_file" \
+        --repo "$GITHUB_REPOSITORY" \
+        -f tagMode="$TAG_MODE"
+else
+    echo "Error: No workflow file found for environment '$env'."
+    exit 1
+fi
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to trigger the provision workflow for '$env'. Please check your permissions and try again."
