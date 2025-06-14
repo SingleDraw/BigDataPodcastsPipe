1057a4bdc99b0f09785cbf8f3b4c3b27aef15b38
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-14 08:25:26 +0200

    [info] readme

--- _test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -a
-# shellcheck disable=SC1091
-source .env
-set +a
-
-echo "PODCAST_INDEX_API_KEY=${PODCASTING_INDEX_API_KEY:-your_api_key}"
-echo "PODCAST_INDEX_API_SECRET=${PODCASTING_INDEX_API_SECRET:-your_api_secret}"
