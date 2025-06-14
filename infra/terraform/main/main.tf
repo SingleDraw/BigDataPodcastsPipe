2ccba62762ffec0990ca70c2b4afe7cf0f4aee1c
Kamil Malek <truckdriverbuck@gmail.com>
2025-06-14 18:05:00 +0200

    [infra] fixes typo in subnet

--- infra/terraform/main/main.tf
+++ infra/terraform/main/main.tf
@@ -456,7 +456,7 @@ resource "azurerm_subnet" "aca_subnet" {
   name                 = "aca-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/23"]
+  address_prefixes     = ["10.0.0.0/23"]
 
   # For ACA internal load balancer, subnet delegation is required:
   delegation {
